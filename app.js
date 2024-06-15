3d0db8efc1adca75eb35ceb000a038cbfb753b7a
kisia0916 <fumiharuabe@gmail.com>
2024-06-16 01:16:04 +0900

    debug

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const http = require("http")
 const server = http.createServer(app)
 
 app.get("/",(req,res)=>{
-    console.log(req.query.token)
-    console.log(req.query)
+    // console.log(req.query.token)
+    // console.log(req.query)
     return res.status(200)
 })
 
